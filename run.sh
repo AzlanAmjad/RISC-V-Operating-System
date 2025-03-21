ff54e7515796b5dcc6935842993b808ede9394ff
Azlan Amjad <azlan.amjad@energytoolbase.com>
2025-03-20 19:23:59 -0600

    chore: reorganize

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
 # compile kernel as kernel.elf
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+$CC $CFLAGS -Wl,-Tsrc/kernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    src/kernel.c
 
 # run QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
