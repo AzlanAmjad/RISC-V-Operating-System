8110b5a18933044803a38096be1ea2f0e2d2be60
Azlan Amjad <azlan.amjad@energytoolbase.com>
2025-03-21 03:05:00 -0600

    feat: add kernel panic

--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,17 @@
 #pragma once
 
-struct sbiret {
+// macro so __FILE__ and __LINE__ are defined properly
+#define PANIC(fmt, ...)                                                                                \
+    do                                                                                                 \
+    {                                                                                                  \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__ /* compiler extension */); \
+        while (1)                                                                                      \
+        {                                                                                              \
+        }                                                                                              \
+    } while (0)
+
+struct sbiret
+{
     long error;
     long value;
 };
